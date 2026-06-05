135a317589c2a13dd36cbd67f68043633f21a58a
Carlos Heredia <carlosheredia2709@gmail.com>
2026-06-05 10:43:16 -0400

    [MOD]: Añadió funcionalidad de entrada y salida para la edad del usuario

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main() {
     string nombre_completo;
+    int edad;
 
     cout << "===================================" << endl;
     cout << "BIENVENIDOS AL PROGRAMA DEL GRUPO 9" << endl;
@@ -13,8 +14,19 @@ int main() {
     cout << "\nPorfavor ingrese su nombre completo: ";
     getline(cin, nombre_completo);
 
+    do{
+        cout << "\nAhora ingrese su edad: ";
+        cin >> edad;
+        if (edad < 0 || edad > 120) {
+            cout << "Por favor ingrese una edad entre 0 y 120." << endl;
+        }
+    }
+    while(edad < 0 || edad > 120);
+
+
     cout << "\nA continuacion se mostaran los datos registrados:\n\n";
     cout << "Nombre: " << nombre_completo << endl;
+    cout << "Edad: " << edad << endl;
 
     cout << "\n<--- Integrantes del Grupo 9 --->\n";
     cout << "\nAngel Enrique Gonzalez Guillen\n";
